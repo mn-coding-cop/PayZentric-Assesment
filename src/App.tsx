519fa7bb9f07ef8fef0395eaeb2e5ee096c63327
Marie Hansen <161481597+mn-coding-cop@users.noreply.github.com>
2024-11-18 10:48:23 -0800

    pdf converter

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PDFContentViewer from "../src/PDFContentViewer/PDFContentViewer";
+
+function App() {
+  return (
+    <div className="App">
+      <PDFContentViewer />
+    </div>
+  );
+}
+
+export default App;
